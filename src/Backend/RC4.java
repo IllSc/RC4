713a5dca5905dbe862a89c5237b9ee835722e2ee
IllSc <ivanmasli@gmail.com>
2014-04-26 21:54:49 +0700

    Tambah GUI

--- src/Backend/RC4.java
+++ src/Backend/RC4.java
@@ -1,3 +1,5 @@
+package Backend;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -45,7 +47,9 @@ public class RC4 {
             
             
         }
+        return message;
     }
+    
 
     public static void main(String[] args) {
 
