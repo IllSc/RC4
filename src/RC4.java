73832752453267f1ead043de049fe6c26895bf83
IllSc <ivanmasli@gmail.com>
2014-04-26 21:01:39 +0700

    Enkripsi hampir kelar

--- src/RC4.java
+++ src/RC4.java
@@ -17,15 +17,34 @@ public class RC4 {
         }
         int j = 0;
         for (int i = 0; i < 256; i++) {
-          
-          j = (j + S[i]+key[i % key.length]) % 256;
-          
-          int temp = S[i];
-          S[i] = S[j];
-          S[j] = temp;
-          
+
+            j = (j + S[i] + key[i % key.length]) % 256;
+
+            int temp = S[i];
+            S[i] = S[j];
+            S[j] = temp;
+
         }
         return S;
+    }
+    /*
+    
+     */
+
+    public int[] encryption(int[] message,int[] S) {
+        int i = 0;
+        int j = 0;
+   
+        for (int k = 0; k < message.length; k++) {
+            i = (i + 1) % 256;
+            j = (j + S[i]) % 256;
+            int temp = S[i];
+            S[i] = S[j];
+            S[j] = temp;
+            int l = (S[i] + S[j]) % 256;
+            
+            
+        }
     }
 
     public static void main(String[] args) {
