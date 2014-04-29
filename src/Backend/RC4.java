866dab0b3a3b330aa47504485816535771abeb9f
Win 7 <Win 7@Windows>
2014-04-30 01:34:45 +0700

    Kelar Backend!!

--- src/Backend/RC4.java
+++ src/Backend/RC4.java
@@ -1,18 +1,19 @@
 package Backend;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Ivan
  */
 public class RC4 {
 
-    public int[] KeyStream(int[] key) {
+    public static int[] KeyStream(int[] key) {
         int[] S = new int[256];
         for (int i = 0; i < 256; i++) {
             S[i] = i;
@@ -33,10 +34,10 @@ public class RC4 {
     
      */
 
-    public int[] encryption(int[] message,int[] S) {
+    public static int[] encryption(int[] message, int[] S) {
         int i = 0;
         int j = 0;
-   
+
         for (int k = 0; k < message.length; k++) {
             i = (i + 1) % 256;
             j = (j + S[i]) % 256;
@@ -44,14 +45,27 @@ public class RC4 {
             S[i] = S[j];
             S[j] = temp;
             int l = (S[i] + S[j]) % 256;
-            
-            
+            message[k] = (message[k] ^ S[l]);
         }
         return message;
     }
-    
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        String key = in.next();
+        String plaintext = in.next();
+        int [] arr = new int [key.length()];
+        for(int i = 0; i<key.length(); i++ ){  
+            arr[i] = (int) key.charAt(i);
+        }
+        int [] arr2 = KeyStream(arr);
+        int [] arr3 = new int [plaintext.length()];
+        for(int i = 0; i< plaintext.length(); i++){
+            arr3[i] = (int)plaintext.charAt(i);
+        }
+        int [] message = encryption(arr3,arr2);
+        for(int i = 0; i<message.length;i++){
+            System.out.print(message[i]+" ");
+        }
     }
 }
