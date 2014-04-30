9ff8fc90eb08b604bf601feb96b67c29ad55f6c6
IllSc <ivanmasli@gmail.com>
2014-04-30 22:55:36 +0700

    Convert File ke array

--- src/Backend/RC4.java
+++ src/Backend/RC4.java
@@ -1,6 +1,10 @@
 package Backend;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,14 +16,28 @@ import java.io.File;
  * @author Ivan
  */
 public class RC4 {
-    
-    private File text;
-    private File key;
-    public RC4(File text,File key){
-        this.text = text;
-        this.key = key;
+
+    private int[] text;
+    private int[] key;
+    private BufferedReader reader;
+
+    public RC4(File text, File key) throws IOException {
+        this.key = toArray(key);
+        this.text = toArray(text);
+        
     }
-    public static int[] KeyStream(int[] key) {
+
+    public int[] toArray(File f) throws FileNotFoundException, IOException {
+        reader = new BufferedReader(new FileReader(f));
+        String key = reader.readLine();
+        int[] arr = new int[key.length()];
+        for (int i = 0; i < key.length(); i++) {
+            arr[i] = (int) key.charAt(i);
+        }
+        return arr;
+    }
+
+    public int[] KeyStream(int[] key) {
         int[] S = new int[256];
         for (int i = 0; i < 256; i++) {
             S[i] = i;
@@ -40,7 +58,7 @@ public class RC4 {
     
      */
 
-    public static int[] encryption(int[] message, int[] S) {
+    public int[] encryption(int[] message, int[] S) {
         int i = 0;
         int j = 0;
 
